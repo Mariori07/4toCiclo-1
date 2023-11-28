3995c43a5a53aeab0fd20d6c35ab159d6576ce95
Mariori07 <152324385+Mariori07@users.noreply.github.com>
2023-11-28 14:05:28 -0500

    Update ProcesosPago.java

--- src/Procesos/ProcesosPago.java
+++ src/Procesos/ProcesosPago.java
@@ -2,17 +2,16 @@ package Procesos;
 import Vista.*;
 import Modelo.*;
 
-public class ProcesosPago {
+public class ProcesosPago{
     public static PagoCuotaMatricula LeerUser(JIFPago f1){
-        PagoCuotaMatricula pc = new PagoCuotaMatricula();
-        Estudiante estudiante = new Estudiante();
-        estudiante.setCodigoEstudiante(f1.txtCodEst.getText());
+        PagoCuotaMatricula pc =new PagoCuotaMatricula();
+       Estudiante estudiante =new Estudiante();
+        estudiante .setCodigoEstudiante(F1.txtCodEst.getTxt());
         return pc;
     }
-    
     public static Estudiante LeerEstudiante(JIFPago f1){
-        Estudiante estudiante = new Estudiante();
-        estudiante.setCodigoEstudiante(f1.txtCodEst.getText());
-        return estudiante;
+        Estudiante estudiante =new Estudiante ();
+        estudiante.setCodigoEstudiante (f1.txtCodEst.getText());
+        return estudiante ;
     }
-}
+        
